0850d00b8e81b05d24e49ae0cecbfd868b031810
temirlan <bolotbekov2288@gmail.com>
2019-12-30 09:26:17 +0600

    tara

---
+++ task14.py
@@ -0,0 +1,50 @@
+# b = int (input('Vvedite vozrast :'))
+# # print([c/2 for c in range(1,b+1) if c/2 ==b])
+# for i in range(1,b+1):
+#     if b % 2 = 0 :
+#     print(b)
+
+# a = input()
+# a = int(a)
+ 
+# even = 0
+# odd = 0
+ 
+# while a > 0:
+#     if a % 2 == 0:
+#         even += 1
+#     else:
+#         odd += 1
+#     a = a // 10
+
+# print("Even: %d, odd: %d" % (even, odd))
+
+# a = []
+# for i in range(6):
+#     n = int(input())
+#     if n%2 == 0:
+#         a.insert(0,n)
+#     else:
+#         a.append(n)
+# print(a)
+
+# num = (input('Vvedite chislo :'))
+#     if num % 2 == 0
+#         print(range(num))
+
+# Учитывая список чисел, найдите и распечатайте все элементы, которые являются четным числом. В
+# В этом случае используйте цикл for, который перебирает список, а не его индексы! Тот
+# есть, не используйте диапазон (на английском языке)
+
+n = ((input("Enter :")).split())
+for i in n:
+    k = int(i)
+    if k % 2 == 0:
+        print(k)
+
+# a = input('Enter: ').split()
+# # b = ''
+# for x in a:
+#     b = int(x)
+#     if b % 2 == 0:
+#         print(b)
